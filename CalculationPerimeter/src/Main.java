896f2d9128b17b2befed120e207ffc48a3c62f99
enveraslan <enver.asln@gmail.com>
2021-06-25 23:57:03 +0300

    Done homework of calculation perimeter and hypotenuse length

---
+++ CalculationPerimeter/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+/**
+ * @author Enver ASLAN, 25/06/2021
+ * This program calculates the perimeter and hypotenuse length of right triangle
+ */
+public class Main {
+    public static void main(String[] args) {
+        double edge1, edge2, hyp, perimeter;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please enter length of first edge: ");
+        edge1 = input.nextDouble();
+
+        System.out.print("Please enter length of second edge: ");
+        edge2 = input.nextDouble();
+
+        hyp = Math.sqrt(edge1 * edge1 + edge2 * edge2);
+        perimeter = edge1 + edge2 + hyp;
+
+        System.out.println("Perimeter: " + perimeter);
+        System.out.println("Length of hypotenuse: " + hyp);
+
+    }
+}
